3399a78b0d43cb0451efe1fa70bb7d107590457e
Julio <julio_denis@live.com>
2020-07-07 14:51:05 -0500

    ListOfCategories fixed, PhotoCards and ListOfPhotoCards components added

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components'
 
 export const List = styled.ul`
-
-    display: flex;
-    overflow: scroll;
-    width: 100%;
+  display: flex;
+  overflow: scroll;
+  width: 100%;
+  overflow: -moz-scrollbars-none;
+-ms-overflow-style: none;
+width: 100%;
+&::-webkit-scrollbar { width: 0 !important }
 `
 
 export const Item = styled.li`
-
-    padding: 0 8px;
-
+  padding: 0 8px;
 `
