0729b3950a69a2ae36f126d53f89e484e29ea160
Julio <julio_denis@live.com>
2020-07-20 19:41:47 -0500

    custom hooks and feature for PhtoCards

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,13 +1,33 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { fadeIn } from '../styles/animation'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
+  ${props => props.fixed && css`
+  {
+    ${fadeIn()}
+    background-color: #fff;
+    border-radius: 60px;
+    box-shadow: 0020px rgba(0, 0, 0, 0.3);
+    left: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    right: 0;
+    top: -20px;
+    transform: scale(.5);
+    z-index: 1;
+  }
+  
+  `}
+   
   overflow: -moz-scrollbars-none;
--ms-overflow-style: none;
-width: 100%;
-&::-webkit-scrollbar { width: 0 !important }
+  -ms-overflow-style: none;
+  width: 100%;
+  &::-webkit-scrollbar { width: 0 !important }
 `
 
 export const Item = styled.li`
