91e8ae08f0fbeb3edf0f8214ca19ec5b36af69f7
Julio <julio_denis@live.com>
2020-07-24 14:05:02 -0500

    navbar components and buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import { Router } from '@reach/router'
 import { GlobalStyle } from './components/styles/GlobalStyle'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
 
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
+      <NavBar />
+
     </div>
   )
 }
