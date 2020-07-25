6fec4b0afe728381395bbf7e55da2225cf9fb9fd
Julio <julio_denis@live.com>
2020-07-24 19:47:40 -0500

    navigation buttons and pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import { NavBar } from './components/NavBar'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisterUser } from './pages/NotRegisterUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -17,6 +24,21 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisterUser path='/favs' />
+                <NotRegisterUser path='/user' />
+              </Router>
+        }
+
+      </UserLogged>
       <NavBar />
 
     </div>
